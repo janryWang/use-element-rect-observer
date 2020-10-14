88e90010587d78e444d3374475ec6a13e283f2b4
janrywang <wangzhili56@126.com>
2020-10-14 14:11:17 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { useEffect, useCallback } from 'react'
+import { useEffect, useCallback, useRef } from 'react'
 import { ElementRectObserver } from './ElementRectObserver'
 
 export const useElementRectObserver = (
@@ -7,12 +7,17 @@ export const useElementRectObserver = (
   contentWindow?: Window
 ) => {
   const observer = useCallback(callback, [])
+  const lastElement = useRef<Element | HTMLElement | HTMLDivElement>()
   useEffect(() => {
     const resizeObserver = new ElementRectObserver((rect) => {
       observer(rect)
     }, contentWindow)
     if (element) {
+      if (lastElement.current && element !== lastElement.current) {
+        resizeObserver.unobserve(lastElement.current)
+      }
       resizeObserver.observe(element)
+      lastElement.current = element
     }
     return () => {
       if (element) {
